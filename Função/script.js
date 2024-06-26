0c0385f34e370b8abdc5dd0e55d16e5ac6c3b5f5
Maverick <159446908+WDiangeles@users.noreply.github.com>
2024-06-26 09:21:48 -0300

    Update script.js Nome do Comandante

--- Função/script.js
+++ Função/script.js
@@ -30,7 +30,7 @@ PUBLIQUE-SE. REGISTRE-SE. CUMPRA-SE.
 
 Monteiro-PB, ${dataEmissaoFormatada}. 
 
-Humberto GERMANO Leite  - TC QOC 
+Saulo TAVARES da Silva – MAJ QOC
 Comandante do 11º BPM`;  
 
     const paragrafos = textoGerado.split('\n');
@@ -93,7 +93,7 @@ PUBLIQUE-SE. REGISTRE-SE. CUMPRA-SE.
 
 Monteiro-PB, ${dataEmissaoFormatada}. 
 
-Humberto GERMANO Leite  - TC QOC 
+Saulo TAVARES da Silva – MAJ QOC
 Comandante do 11º BPM`;  
 
     const paragrafos2 = textoGerado2.split('\n');
